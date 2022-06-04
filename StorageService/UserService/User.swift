b8ef89c468cc8652c919a5ee7b4416e680ea73e2
ArtChemicalCat <93403722+ArtChemicalCat@users.noreply.github.com>
2022-06-04 21:32:33 +0300

    initial file setup

---
+++ StorageService/UserService/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  StorageService
+//
+//  Created by Николай Казанин on 04.06.2022.
+//
+
+import Foundation
---
+++ StorageService/UserService/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  StorageService
+//
+//  Created by Николай Казанин on 04.06.2022.
+//
+
+import Foundation
