ad8ef945a9f6fda1c8893f2e9b02b5102164034d
Nikolay Kazanin <artchemist@yandex.ru>
2022-07-28 11:57:15 +0300

    Задача №1

--- Navigation/Scenes/PostInfo/InfoViewController.swift
+++ Navigation/Scenes/PostInfo/InfoViewController.swift
@@ -6,16 +6,29 @@
 //
 
 import UIKit
+import SnapKit
 
 class InfoViewController: UIViewController {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
     
     let screenWidth = UIScreen.main.bounds.width
     let screenHeight = UIScreen.main.bounds.height
     
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
+        layout()
+//        setupAlertButton()
+        getTitle()
+    }
+    
+    //MARK: - Metods
+    private func setupAlertButton() {
         let alertButton = CustomButton(withTitle: "Удалить пост", action: { [unowned self] in
             showAlert()
         })
@@ -49,4 +62,49 @@ class InfoViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    private func getTitle() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos/5") else { return }
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+            response.statusCode == 200,
+            let data = data else {
+                return
+            }
+            
+            do {
+                guard let jsonObject = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else { return }
+                guard let userID = jsonObject["userId"] as? Int,
+                      let id = jsonObject["id"] as? Int,
+                      let title = jsonObject["title"] as? String,
+                      let completed = jsonObject["completed"] as? Bool else {
+                    print("ошибка при декодировании json")
+                    return
+                }
+                
+                let toDoItem = ToDoItem(userId: userID, id: id, title: title, completed: completed)
+                DispatchQueue.main.async {
+                    self?.setTitle(for: toDoItem)
+                }
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+    
+    private func setTitle(for toDoItem: ToDoItem) {
+        titleLabel.text = toDoItem.title
+    }
+    
+    private func layout() {
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view).offset(16)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide).offset(16)
+        }
+    }
 }
