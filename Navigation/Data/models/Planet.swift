1394764cdcc667dfaf39120eb52b1a21de9a2618
Nikolay Kazanin <artchemist@yandex.ru>
2022-07-28 12:23:12 +0300

    Задача №2

---
+++ Navigation/Data/models/Planet.swift
@@ -0,0 +1,17 @@
+//
+//  Planet.swift
+//  Navigation
+//
+//  Created by Николай Казанин on 28.07.2022.
+//
+
+import Foundation
+
+struct Planet: Codable {
+    let name: String
+    let rotationPeriod: String
+    let orbitalPeriod: String
+    let diameter: String
+    let climate: String
+    let gravity: String
+}
--- Navigation/Scenes/PostInfo/InfoViewController.swift
+++ Navigation/Scenes/PostInfo/InfoViewController.swift
@@ -15,6 +15,12 @@ class InfoViewController: UIViewController {
         return label
     }()
     
+    private let tatooineLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    
     let screenWidth = UIScreen.main.bounds.width
     let screenHeight = UIScreen.main.bounds.height
     
@@ -25,6 +31,7 @@ class InfoViewController: UIViewController {
         layout()
 //        setupAlertButton()
         getTitle()
+        getPlanetInfo()
     }
     
     //MARK: - Metods
@@ -96,15 +103,52 @@ class InfoViewController: UIViewController {
         }.resume()
     }
     
+    private func getPlanetInfo() {
+        guard let url = URL(string: "https://swapi.dev/api/planets/1") else { return }
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+            response.statusCode == 200,
+            let data = data else {
+                return
+            }
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase // CodingKeys не пригодились)
+            do {
+                let tatooine = try decoder.decode(Planet.self, from: data)
+                DispatchQueue.main.async {
+                    self?.setOrbitalPeriod(for: tatooine)
+                }
+            } catch {
+                print(error)
+            }
+            
+        }.resume()
+    }
+    
     private func setTitle(for toDoItem: ToDoItem) {
         titleLabel.text = toDoItem.title
     }
     
+    private func setOrbitalPeriod(for planet: Planet) {
+        tatooineLabel.text = "Период вращения татуина вокруг своей оси составляет \(planet.orbitalPeriod)"
+    }
+    
     private func layout() {
         view.addSubview(titleLabel)
+        view.addSubview(tatooineLabel)
         titleLabel.snp.makeConstraints { make in
             make.top.equalTo(view).offset(16)
             make.leading.trailing.equalTo(view.safeAreaLayoutGuide).offset(16)
+        }
+        
+        tatooineLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(titleLabel)
+            make.top.equalTo(titleLabel.snp.bottom).offset(20)
         }
     }
 }
